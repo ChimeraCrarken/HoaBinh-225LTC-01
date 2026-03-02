6dc17362b3e243fbb02d3bc1afc69342abd2d32a
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-02 14:46:54 +0700

    PushCode Generic

---
+++ Generic/Generic/GenericClassOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class GenericClassOne<T>
+    {
+        private List<T> items = new List<T>();
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public void Display()
+        {
+            foreach (T item in items)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Generic/Generic/main.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class Main1
+    {
+        static void Main(string[] args)
+        {
+            // Generic với int
+            GenericClassOne<int> intList = new GenericClassOne<int>();
+            intList.Add(10);
+            intList.Add(20);
+            intList.Display();
+
+            // Generic với double
+            GenericClassOne<double> doubleList = new GenericClassOne<double>();
+            doubleList.Add(1.5);
+            doubleList.Add(2.8);
+            doubleList.Display();
+
+            // Generic với string
+            GenericClassOne<string> stringList = new GenericClassOne<string>();
+            stringList.Add("Hello");
+            stringList.Add("CSharp");
+            stringList.Display();
+
+            // Generic với object
+            GenericClassOne<object> objectList = new GenericClassOne<object>();
+            objectList.Add(100);
+            objectList.Add("Hi");
+            objectList.Add(3.14);
+            objectList.Display();
+        }
+
+    }
+}
