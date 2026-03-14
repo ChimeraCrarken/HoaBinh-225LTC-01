2cd46d8a99e293cb47ade44b7cf12f316c37d16b
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:09:17 +0700

    Push Bai1_LINQ

---
+++ Bai1_LINQ/Bai1_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        var result = numbers.Where(n => n % 2 == 0);
+
+        Console.WriteLine("Số chẵn:");
+        foreach (var n in result)
+            Console.WriteLine(n);
+    }
+}
