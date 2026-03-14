d0e55c96c685a260966a7b22284903f958b095af
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:11:42 +0700

    Push Bai4_LINQ

---
+++ Bai4_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai4_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 8, 3, 5, 1, 7 };
+
+        var result = numbers.OrderByDescending(n => n);
+
+        Console.WriteLine("Danh sách giảm dần:");
+        foreach (var n in result)
+            Console.WriteLine(n);
+    }
+}
