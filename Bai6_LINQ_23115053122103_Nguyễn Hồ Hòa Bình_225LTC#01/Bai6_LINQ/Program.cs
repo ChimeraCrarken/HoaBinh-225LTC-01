702136ad73036a71433e5a1db0cb352b1549e5d0
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:12:33 +0700

    Push Bai6_LINQ

---
+++ Bai6_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai6_LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+        int count = numbers.Count(n => n % 2 == 0);
+
+        Console.WriteLine("Số chẵn: " + count);
+    }
+}
