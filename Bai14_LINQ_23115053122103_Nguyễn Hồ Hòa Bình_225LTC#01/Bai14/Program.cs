2f20995f9d099d59b5ce239be050aa35b443cd8c
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:15:58 +0700

    Push Bai14_LINQ

---
+++ Bai14_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=6},
+            new Student{Id=3,Name="Chi",Score=9},
+            new Student{Id=4,Name="Dung",Score=7}
+        };
+
+        bool fail = students.Any(s => s.Score < 5);
+
+        Console.WriteLine("Có sinh viên rớt?");
+        Console.WriteLine(fail);
+    }
+}
