edea4cfe92a4333d9d621c9dbf33540536628450
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:17:20 +0700

    Push Bai19_LINQ

---
+++ Bai19_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai19_LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 2, 2, 3, 4, 4 };
+
+        var result = numbers
+            .GroupBy(x => x)
+            .OrderByDescending(g => g.Count())
+            .First().Key;
+
+        Console.WriteLine("Số xuất hiện nhiều nhất trong danh sách là:");
+        Console.WriteLine(result);
+    }
+}
