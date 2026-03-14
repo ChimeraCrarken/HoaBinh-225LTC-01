517786248051e3a9f18e04233acc1be15df79eac
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:16:28 +0700

    Push Bai16_LINQ

---
+++ Bai16_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai16_LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        int sum = numbers.Sum();
+
+        Console.WriteLine("Tổng các số trong danh sách:");
+        Console.WriteLine(sum);
+    }
+}
