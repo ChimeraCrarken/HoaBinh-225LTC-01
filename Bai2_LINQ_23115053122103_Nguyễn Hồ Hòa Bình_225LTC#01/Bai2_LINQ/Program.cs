58ffe0da9e26bd4814aa3497a4984111da70114e
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:10:48 +0700

    Push Bai2_LINQ

---
+++ Bai2_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai2_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        var result = numbers.Where(n => n > 5);
+
+        Console.WriteLine("Số lớn hơn 5:");
+        foreach (var n in result)
+            Console.WriteLine(n);
+    }
+}
