b8c5b338765d92a3dcf0b1cdbe17694e3a24c4b7
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:12:12 +0700

    Push Bai5_LINQ

---
+++ Bai5_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai5_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var result = numbers.Select(n => n * n);
+
+        Console.WriteLine("Danh sách bình phương của các số:");
+        foreach (var n in result)
+            Console.WriteLine(n);
+    }
+}
