d66c28cd248937313fdf078427821a3006e2374a
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:13:04 +0700

    Push Bai7_LINQ

---
+++ Bai7_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai7_LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 11 };
+
+        bool result = numbers.Any(n => n > 10);
+
+        Console.WriteLine("Danh sách có số lớn hơn 10?");
+        Console.WriteLine(result);
+    }
+}
