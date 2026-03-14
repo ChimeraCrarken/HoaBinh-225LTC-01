aea97726345334250c9b862da1708d2e2054f541
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:16:13 +0700

    Push Bai15_LINQ

---
+++ Bai15_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai15_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+        var result = numbers.Distinct();
+
+        Console.WriteLine("Danh sách mới:");
+        foreach (var n in result)
+            Console.WriteLine(n);
+    }
+}
