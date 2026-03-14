03cde4eb8349cc1390fdef6d12d5560eab5fb27f
NguyenHoHoaBinh <winsayia123@gmail.com>
2026-03-15 00:16:43 +0700

    Push Bai17_LINQ

---
+++ Bai17_LINQ_23115053122103_Nguyễn Hồ Hòa Bình_225LTC#01/Bai17_LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="An",Score=8},
+            new Student{Id=2,Name="Binh",Score=6},
+            new Student{Id=3,Name="Chi",Score=9}
+        };
+
+        double avg = students.Average(s => s.Score);
+
+        Console.WriteLine("Điểm trung bình của sinh viên: ");
+        Console.WriteLine(avg);
+    }
+}
